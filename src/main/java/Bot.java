260b9415fd24420010f64b3a8fd53e1bdfd50e51
Roman S <77018012+romanbffff@users.noreply.github.com>
2023-02-24 17:33:44 +0200

    Додано перевірку на інші типи, можливі при відправкі повідомлень, документ/фото

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,13 +1,18 @@
 import org.glassfish.jersey.jaxb.internal.XmlCollectionJaxbProvider;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.games.Animation;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -37,6 +42,31 @@ class Bot extends TelegramLongPollingBot {
 
                 try {
                     execute(sendAnimation);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (update.getMessage().hasPhoto()) {
+                List<PhotoSize> photoSizes = update.getMessage().getPhoto();
+                PhotoSize photo = photoSizes.get(photoSizes.size() - 1);
+
+                SendPhoto sendPhoto = new SendPhoto();
+                sendPhoto.setChatId(chatId.toString());
+                sendPhoto.setPhoto(new InputFile(photo.getFileId()));
+
+                try {
+                    execute(sendPhoto);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (update.getMessage().hasDocument()) {
+                Document document = update.getMessage().getDocument();
+
+                SendDocument sendDocument = new SendDocument();
+                sendDocument.setChatId(chatId.toString());
+                sendDocument.setDocument(new InputFile(document.getFileId()));
+
+                try {
+                    execute(sendDocument);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
